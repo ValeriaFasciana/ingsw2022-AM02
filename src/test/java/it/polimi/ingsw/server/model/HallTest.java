2c70c37d0a057bd9e963ded1ea1a806b76ae8520
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-04-05 14:16:11 +0200

    Add files via upload

---
+++ src/test/java/it/polimi/ingsw/server/model/HallTest.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.server.model.player.playerBoard;
+
+import it.polimi.ingsw.server.model.PawnColour;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HallTest {
+    Hall hall;
+
+    @BeforeEach
+    public void setUp()  {
+        this.hall = new Hall();
+    }
+
+    /**
+     *
+     * It's true if line of given colour reaches 10
+     */
+    @Test
+    void isLineFull() {
+        this.hall.addStudentsForEachColour(9);
+
+        for(PawnColour colour : PawnColour.values()){
+            assertEquals(10, this.hall.getCount(colour)+1);
+        }
+    }
+
+    /**
+     *
+     * It's true if the actual count is equals to the previous one + 1
+     */
+    @Test
+    void addStudents() {
+        this.hall.addStudentsForEachColour(7);
+
+        for(PawnColour colour : PawnColour.values()){
+            this.hall.addStudents(colour);
+            assertEquals(8, hall.getCount(colour));
+        }
+
+    }
+}
