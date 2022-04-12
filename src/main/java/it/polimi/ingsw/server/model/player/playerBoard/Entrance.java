19f4662306f3a7884860f1cf2a4c05ebd5aea3fa
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-04-12 23:24:28 +0200

    Updated constructor

--- src/main/java/it/polimi/ingsw/server/model/player/playerBoard/Entrance.java
+++ src/main/java/it/polimi/ingsw/server/model/player/playerBoard/Entrance.java
@@ -1,11 +1,12 @@
 package it.polimi.ingsw.server.model.player.playerBoard;
 
 import it.polimi.ingsw.server.model.StudentContainer;
+import it.polimi.ingsw.server.model.board.Bag;
 
 public class Entrance extends StudentContainer {
-
+    Bag bag;
     public Entrance(Integer capacity){
         super(capacity);
+        bag.pick(capacity);
     }
-
 }
