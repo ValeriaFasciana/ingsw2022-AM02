e3255994b9aecf89c7ca210e1f138d94d928f9c7
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-04-26 16:13:25 +0200

    added test classes and minor fixes

--- src/test/java/it/polimi/ingsw/server/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/server/model/GameTest.java
@@ -18,20 +18,26 @@ class GameTest {
     HashMap<String,Player> playerMap = new HashMap<>();
     GameBoard gameBoard =new GameBoard(3,12,3);
     ArrayList<Player> playingOrder;
+    Player[] playerOrder;
 
      @BeforeEach
     private void setUp() throws IOException {
         Player player1 = new Player("player1",3,8, TowerColour.BLACK,1);
         Player player2 = new Player("player2",3,8, TowerColour.WHITE,1);
+        Player player3 = new Player("player3",3,8, TowerColour.GREY,1);
         this.playerMap.put("player1",player1);
         this.playerMap.put("player2",player2);
-        this.game = new Game(this.playerMap, 2, false);
+        this.playerMap.put("player3",player2);
+
+        this.game = new Game(this.playerMap,3, false);
+        this.playerOrder = new Player[game.getNumberOfPlayers()];
+        this.playingOrder = new ArrayList<>();
     }
 
     @Test
     void addPlayer() throws IOException {
-        this.game.addPlayer("player3", TowerColour.GREY,1);
-        assertEquals(TowerColour.GREY, this.game.getPlayers().get("player3").getTowerColour());
+        this.game.addPlayer("player4", TowerColour.GREY,1);
+        assertEquals(TowerColour.GREY, this.game.getPlayers().get("player4").getTowerColour());
     }
 
     @Test
@@ -64,11 +70,9 @@ class GameTest {
     @Test
     void updateTurnWithPlayedAssistant() {
     }
-
     @Test
     void startPlanningPhase() {
     }
-
     @Test
     void assignProfessors() {
     }
@@ -86,33 +90,50 @@ class GameTest {
 
     @Test
     void getNumberOfPlayers() {
-         assertEquals(2, game.getNumberOfPlayers());
+         assertEquals(3, game.getNumberOfPlayers());
     }
 
     @Test
     void getSettings() {
-         assertEquals(3, game.getSettings().getStudentsInClouds());
+         assertEquals(4, game.getSettings().getStudentsInClouds());
     }
 /*
     @Test
     void getProfessorMap() {
 
     }
-
+*/
     @Test
     void initPlayingOrder() {
+        this.game.initPlayingOrder();
+        playingOrder = this.game.getPlayingOrder();
+        assertEquals(true, this.playingOrder.contains(this.game.getPlayers().get("player1")));
     }
-
+    /*
     @Test
     void setPlanningOrder() {
-    }
 
+        this.game.initPlayingOrder();
+        AssistantCard card1 = new AssistantCard(2, 1);
+        AssistantCard card2 = new AssistantCard(1,2);
+        AssistantCard card3 = new AssistantCard(3,2);
+
+        game.getPlayers().get("player1").setChosenAssistant(card1);
+        game.getPlayers().get("player2").setChosenAssistant(card2);
+        game.getPlayers().get("player3").setChosenAssistant(card3);
+        game.setActionOrder();
+        this.game.setPlanningOrder();
+        assertEquals(game.getPlayers().get("player2"), game.getPlayingOrder().get(0));
+    }
+*/
     @Test
     void setActionOrder() {
+        this.game.initPlayingOrder();
+        AssistantCard card1 = new AssistantCard(2, 1);
+        AssistantCard card2 = new AssistantCard(1,2);
+        game.getPlayers().get("player1").setChosenAssistant(card1);
+        game.getPlayers().get("player2").setChosenAssistant(card2);
+        game.setActionOrder();
+        assertEquals(game.getPlayers().get("player2"), game.getPlayingOrder().get(0));
     }
-
-    @Test
-    void getPlayingOrder() {
-    }
-    */
 }
