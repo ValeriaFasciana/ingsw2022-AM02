3614195a8a26a6779076631bddde76c1538a3781
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-05-03 14:08:19 +0200

    added test classes and minor fixes

--- src/test/java/it/polimi/ingsw/server/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/server/model/GameTest.java
@@ -17,58 +17,83 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
     static Game game;
 
-    @BeforeAll
-    static void setUp() throws IOException {
-        Map<String,TowerColour> players = new HashMap<>();
-        players.put("player1",TowerColour.BLACK);
-        players.put("player2",TowerColour.WHITE);
-        players.put("player3",TowerColour.GREY);
-        game = new Game(players,3, false);
+
+    @BeforeEach
+    private void setUp () throws IOException {
+//            Player player1 = new Player("player1", 3, 8, TowerColour.BLACK, 1);
+//            Player player2 = new Player("player2", 3, 8, TowerColour.WHITE, 1);
+//            Player player3 = new Player("player3", 3, 8, TowerColour.GREY, 1);
+//            this.playerMap.put("player1", player1);
+//            this.playerMap.put("player2", player2);
+//            this.playerMap.put("player3", player3);
 
     }
 
     @Test
-    void addPlayer() throws IOException {
-        this.game.addPlayer("player4", TowerColour.GREY);
-        assertEquals(TowerColour.GREY, this.game.getPlayers().get("player4").getTowerColour());
-    }
-
-    @Test
-    void moveMotherNature() {
-         this.game.moveMotherNature(4);
-         assertEquals(this.game.getGameBoard().getIsleCircle().get(4), this.game.getMotherNaturePosition());
-    }
-
-    @Test
-    void getPlayableAssistants() {
-        Set<Integer> playableCards = this.game.getPlayableAssistants();
-        assertEquals(game.getPlayers().get("player1").getDeck().keySet(),playableCards);
-    }
-
-    @Test
-    void playAssistantCardAndEndTurn() {
+    void playAssistantCardAndEndTurn () {
         game.playAssistantCard(3);
-        assertEquals(1,game.getCurrentRound().getActionOrder().size());
+        assertEquals(1, game.getCurrentRound().getActionOrder().size());
         this.game.endCurrentPlayerTurn();
         assertEquals(this.game.getPlayers().get("player2"), this.game.getCurrentPlayer());
     }
 
 
     @Test
-    void getMotherNaturePosition() {
+    void getMotherNaturePosition () {
         this.game.moveMotherNature(4);
         assertEquals(this.game.getGameBoard().getIsleCircle().get(4), this.game.getMotherNaturePosition());
     }
 
     @Test
-    void getPlayers() {
+    void getPlayers () {
         assertEquals(TowerColour.BLACK, game.getPlayers().get("player1").getTowerColour());
     }
 
     @Test
-    void getGameBoard() {
-         IsleGroup isleGroup = game.getGameBoard().getMotherNaturePosition();
-         assertEquals(isleGroup, game.getGameBoard().getMotherNaturePosition());
+    void getGameBoard () {
+        IsleGroup isleGroup = game.getGameBoard().getMotherNaturePosition();
+        assertEquals(isleGroup, game.getGameBoard().getMotherNaturePosition());
     }
 
+
+
+
+    /*
+    @Test
+    void setPlanningOrder() {
+
+        this.game.initPlayingOrder();
+        AssistantCard card1 = new AssistantCard(2, 1);
+        AssistantCard card2 = new AssistantCard(1,2);
+        AssistantCard card3 = new AssistantCard(3,2);
+
+        game.getPlayers().get("player1").setChosenAssistant(card1);
+        game.getPlayers().get("player2").setChosenAssistant(card2);
+        game.getPlayers().get("player3").setChosenAssistant(card3);
+        game.setActionOrder();
+        this.game.setPlanningOrder();
+        assertEquals(game.getPlayers().get("player2"), game.getPlayingOrder().get(0));
+    }
+*/
+    /*
+    @Test
+    void setActionOrder() {
+        this.game.initPlayingOrder();
+        ArrayList<Player> playingOrderRandom = new ArrayList<>();
+        for (Player player :this.game.getPlayingOrder()) {
+
+        }
+
+        AssistantCard card1 = new AssistantCard(2, 1);
+        AssistantCard card2 = new AssistantCard(2,1);
+        AssistantCard card3 = new AssistantCard(1,1);
+        game.getPlayers().get("player1").setChosenAssistant(card1);
+        game.getPlayers().get("player2").setChosenAssistant(card2);
+        game.getPlayers().get("player3").setChosenAssistant(card3);
+        game.setActionOrder();
+        assertEquals(game.getPlayers().get("player1"), game.getPlayingOrder().get(1));
+    }
+
+     */
 }
+
---
+++ src/test/java/it/polimi/ingsw/server/model/TurnTest.java
@@ -0,0 +1,70 @@
+package it.polimi.ingsw.server.model;
+
+import it.polimi.ingsw.server.model.player.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnTest {
+    Turn turn;
+    Player player;
+    ArrayList<AssistantCard> playedCards;
+    ArrayList<Player> orderedPlayers;
+    AssistantCard card1;
+    AssistantCard card2;
+
+
+    @BeforeEach
+    private void setUp() throws IOException {
+        this.player = new Player("player1", 3, 3, TowerColour.WHITE, 3);
+        this.turn = new Turn(Phase.ACTION, player);
+        this.card1 = new AssistantCard(1, 1);
+        this.card2 = new AssistantCard(2, 2);
+        this.orderedPlayers = new ArrayList<>();
+    }
+
+    @Test
+    void getCurrentPhase() {
+        assertEquals(Phase.ACTION, turn.getCurrentPhase());
+    }
+
+    @Test
+    void getCurrentPlayer() {
+        assertEquals(player, turn.getCurrentPlayer());
+    }
+
+    @Test
+    void setCurrentPhase() {
+        turn.setCurrentPhase(Phase.PLANNING);
+        assertEquals(Phase.PLANNING, turn.getCurrentPhase());
+    }
+
+    @Test
+    void setCurrentPlayer() throws IOException {
+        Player player2 = new Player("player2", 3, 3, TowerColour.BLACK, 3);
+        turn.setCurrentPlayer(player2);
+        assertEquals(player2, turn.getCurrentPlayer());
+    }
+
+    @Test
+    void addplayedCards() {
+       this.turn.addplayedCards(card1);
+        assertEquals(card1, turn.getPlayedCards().get(0));
+    }
+    @Test
+    void getPlayedCards() {
+        this.turn.addplayedCards(card1);
+        assertEquals(card1, turn.getPlayedCards().get(0));
+    }
+/*
+    @Test
+    void updateWithPlayedAssistant() throws  IOException {
+        this.turn.updateWithPlayedAssistant(player);
+        assertEquals(card1, orderedPlayers.get(0).getChosenAssistant());
+    }
+*/
+}
