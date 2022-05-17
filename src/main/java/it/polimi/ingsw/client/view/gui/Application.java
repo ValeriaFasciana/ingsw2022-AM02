b2381d20aee9ffe1d7735248ea363b84d2809931
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-05-17 17:01:30 +0200

    added gui classes

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/Application.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.client.view.gui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Application extends javafx.application.Application {
+
+
+    @Override
+    public void start(Stage primaryStage)throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("startNewGame.fxml"));
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.minHeightProperty().setValue(400);
+        primaryStage.minWidthProperty().setValue(600);
+        primaryStage.setTitle("Start New Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+}
