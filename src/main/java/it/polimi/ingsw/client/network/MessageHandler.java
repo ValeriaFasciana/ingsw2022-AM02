af0391b4757e1300e74a2f2d3ca6d26faedc056f
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-05-19 21:57:16 +0200

    added messages classes

---
+++ src/main/java/it/polimi/ingsw/client/network/MessageHandler.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.client.network;
+
+import it.polimi.ingsw.shared.messages.Message;
+import it.polimi.ingsw.shared.messages.Type;
+import it.polimi.ingsw.shared.messages.fromClientToServer.*;
+import it.polimi.ingsw.shared.messages.fromServerToClient.*;
+
+public class MessageHandler {
+}
