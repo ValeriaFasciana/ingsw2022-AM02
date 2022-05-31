3bf8b0f5badf965745787d1aa706ccb305dc70d6
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-05-31 18:18:50 +0200

    added and modified CLI classes

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/GUI.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.client.view.gui;
+
+import it.polimi.ingsw.client.ServerHandler;
+import it.polimi.ingsw.client.view.ViewInterface;
+
+public class GUI implements ViewInterface {
+    @Override
+    public void askConnectionParameters() {
+
+    }
+
+
+    @Override
+    public void waiting() {
+
+    }
+
+    @Override
+    public void selectMotherNatureDestination() {
+
+    }
+
+    @Override
+    public void selectCard() {
+
+    }
+
+    @Override
+    public void selectStudentToMove() {
+
+    }
+
+    @Override
+    public void selectStudentDestination() {
+
+    }
+
+    @Override
+    public void displayMessage(String message) {
+
+    }
+
+    @Override
+    public void askLobbyInfo() {
+
+    }
+
+    @Override
+    public void setServerHandler(ServerHandler serverHandler) {
+
+    }
+}
