37f4476e52682f34b7a3d94576abacc4c2ba9b58
riccardocora <riccardo.cora@mail.polimi.it>
2022-06-03 16:40:04 +0200

    cli implementations
    model fixing + cli data flow implementations

--- src/main/java/it/polimi/ingsw/network/messages/clienttoserver/events/MoveStudentToHallResponse.java
+++ src/main/java/it/polimi/ingsw/network/messages/clienttoserver/events/MoveStudentToHallResponse.java
@@ -1,11 +1,7 @@
 package it.polimi.ingsw.network.messages.clienttoserver.events;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonGetter;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import it.polimi.ingsw.server.ServerMessageVisitor;
 import it.polimi.ingsw.shared.enums.PawnColour;
 import it.polimi.ingsw.network.messages.MessageFromClientToServer;
@@ -14,9 +10,8 @@ import it.polimi.ingsw.network.messages.Type;
 public class MoveStudentToHallResponse extends MessageFromClientToServer {
     PawnColour studentColour;
     @JsonCreator
-    public MoveStudentToHallResponse(@JsonProperty("playerNickName") String username, @JsonProperty("studentColour") PawnColour studentColour) {
-        super(username,Type.CLIENT_RESPONSE);
-
+    public MoveStudentToHallResponse(@JsonProperty("username") String username,@JsonProperty("studentColour") PawnColour studentColour) {
+        super(username, Type.CLIENT_RESPONSE);
         this.studentColour = studentColour;
     }
 
