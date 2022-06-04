a3931e6ad5b378e6737d3f8c071df31dc53e0ca1
Valeria Fasciana <100208529+ValeriaFasciana@users.noreply.github.com>
2022-06-04 15:53:08 +0200

    added and modified CLI classes

---
+++ src/main/java/it/polimi/ingsw/client/view/cli/graphics/GraphicalCloud.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.view.cli.graphics;
+
+public class GraphicalCloud {
+}
--- src/main/java/it/polimi/ingsw/client/view/cli/graphics/GraphicalIsland.java
+++ src/main/java/it/polimi/ingsw/client/view/cli/graphics/GraphicalIsland.java
@@ -3,57 +3,85 @@ package it.polimi.ingsw.client.view.cli.graphics;
 public class GraphicalIsland extends GraphicalElement{
 
     private final int squareHeight = 4;
-    private final int squareWidth = 5;
+    private final int squareWidth = 10;
 
     public GraphicalIsland() {
-        super(18, 5);
+        super(200, 5);
     }
 
     //è da creare metodo per disegnare le isole a seconda se sono ancora indipendenti o unite
     /**
      * Draw a single square of island
-     * @param x the x coord where the square has to be drawn
-     * @param y the y coord where the square has to be drawn
+     * @param x the x coord for height
+     * @param y the y coord for width
      */
-    private void drawSquare(int x, int y) {
-        char[] boxChars = getChars();
-        char upLeft = boxChars[0];
-        char upRight = boxChars[1];
-        char horizontal = boxChars[2];
-        char bottomRight = boxChars[3];
-        char bottomLeft = boxChars[4];
-        char vertical = boxChars[5];
+
+    public void drawIslandInfo(int x, int y) {
+        char i = '2';
+        symbols[x + 2][y + 1] = 'S';
+        symbols[x + 2][y + 2] = 'I';
+        symbols[x + 2][y + 3] = 'Z';
+        symbols[x + 2][y + 4] = 'E';
+        symbols[x + 2][y + 5] = ':';
+        symbols[x + 2][y + 6] = i;
+
+    }
+
+    public void drawStudent(int x, int y) {
+
+    }
+    public void drawTower(int x, int y) {
+        symbols[x + 1][y + squareWidth -3] = 'T';
+        colours[x + 1][y + squareWidth -3] = Colour.ANSI_BRIGHT_BLACK;
+        backGroundColours[x + 1][y + squareWidth -2] = BackGroundColour.ANSI_DEFAULT;
+
+    }
+    public void drawMotherNature(int x, int y){
+        symbols[x+1][y +2] = 'M';
+        colours[x+1][y +2] = Colour.ANSI_BRIGHT_RED;
+    }
+
+    public void drawIsland(int x, int y) {
 
         for (int i = 0; i < squareHeight; i++) {
             for (int j = 0; j < squareWidth; j++) {
-                if (i == 0 && j == 0)
-                    symbols[i + x][j + y] = upLeft;
-                else if (i == 0 && j == squareWidth - 1)
-                    symbols[i + x][j + y] = upRight;
-                else if (i == squareHeight - 1 && j == 0)
-                    symbols[i + x][j + y] = bottomLeft;
-                else if (i == squareHeight - 1 && j == squareWidth - 1)
-                    symbols[i + x][j + y] = bottomRight;
-                else if (i == 0 || i == squareHeight - 1)
-                    symbols[i + x][j + y] = horizontal;
-                else if (j == 0 || j == squareWidth - 1)
-                    symbols[i + x][j + y] = vertical;
+                if (i == 0 && j == 0){
+                    symbols[i + x][j + y] = '▋';
+                    colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
+                else if (i == 0 && j == squareWidth - 1){
+                    symbols[i + x][j + y] = '▋';
+                    colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
+                else if (i == squareHeight - 1 && j == 0){
+                    symbols[i + x][j + y] = '▋';
+                    colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
+                else if (i == squareHeight - 1 && j == squareWidth - 1){
+                    symbols[i + x][j + y] = '▋';
+                    colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
+                else if (i == 0 || i == squareHeight - 1) {
+                    symbols[i + x][j + y] = '▋';
+                colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
+                else if (j == 0 || j == squareWidth - 1){
+                    symbols[i + x][j + y] = '▋';
+                    colours[i + x][j + y] = Colour.ANSI_BRIGHT_GREEN;}
             }
         }
     }
-    /**
-     * Returns the symbols to be used when drawing a square
-     * @return an array of four chars to be used in the corners of the square
-     */
-    private char[] getChars() {
-        char [] boxChars = new char[6];
-        boxChars[0] = '┌';
-        boxChars[1] = '┐';
-        boxChars[2] = '─';
-        boxChars[3] = '┘';
-        boxChars[4] = '└';
-        boxChars[5] = '│';
 
-        return boxChars;
+    public static void main(String[] args) {
+        GraphicalIsland island = new GraphicalIsland();
+        island.reset();
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < 12; i++) {
+            island.drawIsland(x,y);
+            island.drawMotherNature(x,y);
+            island.drawTower(x,y);
+            island.drawIslandInfo(x,y);
+            y+= 15;
+
+        }
+
+        island.display();
     }
+
 }
